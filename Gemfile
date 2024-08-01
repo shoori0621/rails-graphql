ab075f03483c129c244113959e32a347ae6befd1
shoori0621 <shoori0621@gmail.com>
2024-08-01 21:37:12 +0900

    use redis cache store

--- Gemfile
+++ Gemfile
@@ -27,7 +27,9 @@ gem "stimulus-rails"
 gem "jbuilder"
 
 # Use Redis adapter to run Action Cable in production
-# gem "redis", ">= 4.0.1"
+gem "redis", ">= 4.0.1"
+# redisの高速化
+gem "hiredis"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,9 +23,10 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+    config.cache_store = config.cache_store = :redis_cache_store, {
+      host: ENV['REDIS_HOST'],
+      port: ENV['REDIS_PORT'],
+      driver: :hiredis
     }
   else
     config.action_controller.perform_caching = false
